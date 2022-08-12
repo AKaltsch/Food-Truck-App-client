0b369a13ec7cd8fa96f9e6721db8002ab13deede
Alec Kaltschmid <kaltschmidalec@gmail.com>
2022-08-11 21:36:23 -0700

    selected, infobox, and draggable added to markers

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,6 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useRef } from "react";
 import MapStyles from "../MapStyles";
+import { formatRelative } from "date-fns";
 import {
   GoogleMap,
   useJsApiLoader,
@@ -27,6 +28,7 @@ function Map() {
   const [libraries] = useState(["places"]);
 
   const [markers, setMarkers] = useState([]);
+  const [selected, setSelected] = useState(null);
 
   const { isLoaded, loadError } = useJsApiLoader({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
@@ -39,9 +41,27 @@ function Map() {
     setMap(map);
   }, []);
 
+  ////same as onLoad function ---> Look into diffference btwn the 2!!!!!!!
+  // const onMapLoad = useCallback((map) => {
+  //   mapRef.current = map;
+  // }, []);
+
   const onUnmount = useCallback(function callback(map) {
     setMap(null);
   }, []);
+
+  const onMapClick = useCallback((e) => {
+    setMarkers((marks) => [
+      ...marks,
+      {
+        lat: e.latLng.lat(),
+        lng: e.latLng.lng(),
+        time: new Date(),
+      },
+    ]);
+  }, []);
+
+  const mapRef = useRef();
 
   if (loadError) {
     return "Error loading maps";
@@ -59,24 +79,30 @@ function Map() {
         onLoad={onLoad}
         onUnmount={onUnmount}
         options={options}
-        onClick={(e) => {
-          setMarkers((marks) => [
-            ...marks,
-            {
-              lat: e.latLng.lat(),
-              lng: e.latLng.lng(),
-              time: new Date(),
-            },
-          ]);
-        }}
+        onClick={onMapClick}
       >
         {markers.map((marker) => (
           <Marker
             key={marker.time.toISOString()}
             position={{ lat: marker.lat, lng: marker.lng }}
             draggable={true}
+            onClick={() => {
+              setSelected(marker);
+            }}
           />
         ))}
+
+        {selected ? (
+          <InfoWindow
+            position={{ lat: selected.lat, lng: selected.lng }}
+            onCloseClick={() => setSelected(null)}
+          >
+            <div>
+              <h2>testing infobox</h2>
+              <p>{formatRelative(selected.time, new Date())}</p>
+            </div>
+          </InfoWindow>
+        ) : null}
       </GoogleMap>
     </div>
   );
