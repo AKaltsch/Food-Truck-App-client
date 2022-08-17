73229b49db99d2477ce0a557e197392f14b4445b
Alec Kaltschmid <kaltschmidalec@gmail.com>
2022-08-17 15:06:57 -0700

    login form and css set up... waiting for backend routes

--- src/components/auth-components/Login.js
+++ src/components/auth-components/Login.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
+import "../../form-styles.css";
+
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div>
       <h1>Login</h1>
-      <br />
+      <form>
+        <div className="form-control">
+          <label>E-Mail</label>
+          <input
+            className=""
+            type="email"
+            name="email"
+            id="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <br />
+        <div className="form-control">
+          <label>Password</label>
+          <input
+            className=""
+            type="password"
+            name="password"
+            id="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+      </form>
       <NavLink to="/signup">Create an Account</NavLink>
     </div>
   );
