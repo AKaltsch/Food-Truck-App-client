7640cc5b22507ee0c7466a45cd7f6e0282f9d34c
Alec Kaltschmid <kaltschmidalec@gmail.com>
2022-08-23 20:27:49 -0700

    working on getting csrf to work

--- src/App.js
+++ src/App.js
@@ -4,22 +4,19 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  // const getLogin = axios.get("http://localhost:4000/login").then((res) => {
-  //   console.log(res);
-  // });
-  //
+  const getLogin = axios.get("http://localhost:4000/login").then((res) => {
+    console.log(res);
+  });
 
   useEffect(() => {
-    const token = localStorage.getItem("jwt");
-
-    fetch("http://localhost:4000/login").then((res) => {
-      if (res.ok) {
+    axios.get("http://localhost:4000/login").then((res) => {
+      if (!res) {
+        console.log("unsuccessful");
+      } else {
         console.log("SUCCESS!!!");
         console.log(res);
-      } else {
-        console.log("unsuccessful");
       }
     });
   }, []);
